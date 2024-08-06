d93c7d2cfdf50134a2faada64d7d0996c659cffd
sreeramg1988 <sreeramg1988@gmail.com>
2024-08-06 11:08:09 -0300

    Program to check whether the number is odd or even

---
+++ tests/Week 1/oddEvenCheck.js
@@ -0,0 +1,12 @@
+
+let inputNumber = 25;
+
+function oddEven(inputNum) {
+    if (inputNum % 2 == 0) {
+        console.log(`${inputNum} is an even number`)
+    } else {
+        console.log(`${inputNum} is an odd number`)
+    }
+}
+
+oddEven(inputNumber);
