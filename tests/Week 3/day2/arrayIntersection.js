441e6376b0eaadd087f14f3d555439a6ab459b87
sreeramg1988 <sreeramg1988@gmail.com>
2024-08-22 19:52:42 -0300

    Minor formatting changes

--- tests/Week 3/day2/arrayIntersection.js
+++ tests/Week 3/day2/arrayIntersection.js
@@ -5,20 +5,17 @@
 
 
 function intersection(arrayOne, arrayTwo) {
-
     let interSectionArray = [];
     for (let i = 0; i < arrayOne.length; i++) {
         for (let j = 0; j < arrayTwo.length; j++) {
             //checks whether element in arrayOne is in arrayTwo
             //Also skips adding the common element if it's already present
             if(arrayOne[i] == arrayTwo[j] && !interSectionArray.includes(arrayOne[i])) { 
-
                 interSectionArray.push(arrayOne[i]);
             } 
         }
     }
     return interSectionArray;
-
 }
 
 const caseOne = [1, 2, 3, 5, 9,5];
